f09d562b32f6d3777900fb9489e6298a4fd95d46
Matthew DuBois <147000029+duboimd@users.noreply.github.com>
2024-11-19 09:31:16 -0700

    Create For Loops.py

---
+++ For Loops.py
@@ -0,0 +1,40 @@
+#For Loops Practice
+
+print("Question 1:")
+#1. Write a for loop that prints numbers from the range of 0-10 (inclusive)
+
+print()
+print("Question 2:")
+#2. Write a for loop that prints numbers from the range of 5-25 (inclusive)
+
+print()
+print("Question 3:")
+#3. Write a for loop that prints numbers from the range of -100 to 100 with a skip of 10 (inclusive)
+
+print()
+print("Question 4:")
+#4. Write a for loop that prints numbers from the range of 50 to 500 with a skip of 25 (inclusive)
+
+print()
+print("Question 5:")
+# 5: Create a variable called favDessert and set it equal to your favourite dessert. Then create a for loop to print out each individual letter of favDessert.
+
+print()
+print("Question 6:")
+#6. Write a Python program that will use a for loop to print your name 10 times, and then the word ``Done'' at the end.
+
+print()
+print("Question 7:")
+#7. Write a Python program that will use a for loop to print “Red” and then “Gold” 20 times. (Red Gold Red Gold Red Gold... all on separate lines.)
+
+print()
+print("Question 8:")
+#8. Write a Python program that will use a for loop to print the even numbers from 2 to 100, inclusive.
+
+print()
+print("Question 9:")
+#9. Write a Python program that will use a for loop to print the odd numbers from 1 to 100, inclusive.
+
+print()
+print("Question 10:")
+#10. Ask the user for a range (upper and lower bound), print the sum of numbers within that range
